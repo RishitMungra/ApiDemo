658c54a44f58e6628ab69208c7103c7a6e1ed63b
RishitMungra <mungrarishit@gmail.com>
2024-02-22 19:36:41 +0530

    Fluent Validation

--- ApiDemo/Validation/PersonValidator.cs
+++ ApiDemo/Validation/PersonValidator.cs
@@ -15,15 +15,20 @@ namespace APIDemo.Validation
                 .WithMessage("Name Can't Be Empty");
 
             RuleFor(PersonModel => PersonModel.Email)
-                .NotNull()
-                .WithMessage("Email Can't Be Null")
-                .EmailAddress()
-                .WithMessage("Enter Valid Email Address");
+                .NotEmpty()
+                .WithMessage("Email can't be empty")
+                .MaximumLength(100)
+                .WithMessage("Email must be less than or equal to 100 characters")
+                .Matches(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
+                .WithMessage("Enter a valid email address");
+
 
             RuleFor(PersonModel => PersonModel.Contact)
-                .NotNull()
-                .WithMessage("Enter Contact Number")
-                .Length(10);
+                .NotEmpty()
+                .WithMessage("Contact number can't be empty")
+                .Matches(@"^[0-9]{10}$")
+                .WithMessage("Enter a valid 10-digit contact number");
+
         }
     }
 }
